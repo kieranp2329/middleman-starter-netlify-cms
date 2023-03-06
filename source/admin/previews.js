263db71ba6822715a136299b57f0072f2d3719f8
kieranp2329 <59579538+kieranp2329@users.noreply.github.com>
2023-03-06 16:41:22 +0000

    Update previews.js

--- source/admin/previews.js
+++ source/admin/previews.js
@@ -30,10 +30,10 @@ const ProductPreview = ({ entry, widgetFor }) => {
         <div className="meta">
           <h1>{data.title}</h1>
           <div className="weight">
-            <span>{`${data.weight}kg`}</span>
+            <span>{`Rarity ${data.weight} `}</span>
           </div>
           <div className="price">
-            <span>{`€${data.price}`}</span>
+            <span>{`Estimated Price: ${data.price}`}</span>
           </div>
         </div>
         <div className="body">{widgetFor('body')}</div>
