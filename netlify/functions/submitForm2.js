4885a6d33176be47f059a49a2e463cebb4106320
kieranp2329 <59579538+kieranp2329@users.noreply.github.com>
2023-05-22 13:49:12 +0100

    Update submitForm2.js

--- netlify/functions/submitForm2.js
+++ netlify/functions/submitForm2.js
@@ -3,19 +3,22 @@ exports.handler = async (event, context) => {
     // Parse form data
     const { your_name, your_email, Phone_number, bid_amount, lot_number } = JSON.parse(event.body);
 
-    // Log the form data
-    console.log('Form Data:', {
-      Name: your_name,
-      Email: your_email,
-      Phone_Number: Phone_number,
-      Bid_Amount: bid_amount,
-      Lot_Number: lot_number,
-    });
+    // Log the form data in a formatted way
+    console.log(`Form Data:
+      Name: ${your_name}
+      Email: ${your_email}
+      Phone Number: ${Phone_number}
+      Bid Amount: ${bid_amount}
+      Lot Number: ${lot_number}
+    `);
+
+    // Create the popup message
+    const popupMessage = `Your bid for ${bid_amount} on lot number ${lot_number} has been saved`;
 
     // Return a success response
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Form data received successfully' }),
+      body: JSON.stringify({ message: 'Form data received successfully', popupMessage }),
     };
   } catch (error) {
     // Return an error response
