821d38ac006cd62f9c8ee967a98e67632bc26313
kieranp2329 <59579538+kieranp2329@users.noreply.github.com>
2023-05-24 10:59:27 +0100

    Create main.js

---
+++ data/products/main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the search input element
+  const searchInput = document.querySelector('#search-input');
+
+  // Get all the product elements
+  const products = document.querySelectorAll('.product');
+
+  // Function to handle search
+  const handleSearch = function() {
+    const searchTerm = searchInput.value.toLowerCase();
+
+    products.forEach(function(product) {
+      const title = product.querySelector('.title').textContent.toLowerCase();
+
+      if (title.includes(searchTerm)) {
+        product.style.display = 'block';
+      } else {
+        product.style.display = 'none';
+      }
+    });
+  };
+
+  // Attach event listener to search input
+  searchInput.addEventListener('input', handleSearch);
+});
