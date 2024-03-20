a7a65bc6b184a65f571c1eca63cb0f7b179f1729
kieranp2329 <59579538+kieranp2329@users.noreply.github.com>
2024-03-20 16:09:39 +0000

    Update carousel.js

--- source/carousel.js
+++ source/carousel.js
@@ -1,18 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Function to handle carousel slide movement
-    function moveSlide(n) {
-      var slides = document.querySelector(".carousel .slides");
-      var slideWidth = slides.clientWidth;
-      slides.scrollLeft += n * slideWidth;
-    }
+// Function to handle carousel slide movement
+function moveSlide(n) {
+    var slides = document.querySelector(".carousel .slides");
+    var slideWidth = slides.clientWidth;
+    slides.scrollLeft += n * slideWidth;
+  }
   
-    // Event delegation for prev and next buttons
-    document.addEventListener('click', function(event) {
-      if (event.target.classList.contains('prev')) {
-        moveSlide(-1);
-      } else if (event.target.classList.contains('next')) {
-        moveSlide(1);
-      }
-    });
+  // Event delegation for prev and next buttons
+  document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('prev')) {
+      moveSlide(-1);
+    } else if (event.target.classList.contains('next')) {
+      moveSlide(1);
+    }
   });
   
