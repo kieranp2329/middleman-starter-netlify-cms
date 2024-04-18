c8be62affcfb5f77f170810911e412cd4157f52c
kieranp2329 <59579538+kieranp2329@users.noreply.github.com>
2024-04-18 10:55:32 +0100

    Remove nav button call in search function

--- source/searchproducts.js
+++ source/searchproducts.js
@@ -23,14 +23,4 @@ document.addEventListener('DOMContentLoaded', function() {
   // Attach event listener to search input
   searchInput.addEventListener('input', handleSearch);
 
-  // Integrated navigation function
-  (function() {
-    var t, e;
-    t = document.querySelector('.nav-button');
-    e = document.querySelector('.navigation');
-    t.addEventListener('click', function() {
-      return e.classList.toggle('open');
-      t.classList.toggle('active');
-    }, false);
-  }).call(this);
 });
