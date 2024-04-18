53138310a50e011fa729414e8570c15b350c64bc
kieranp2329 <59579538+kieranp2329@users.noreply.github.com>
2024-04-18 13:41:12 +0100

    FUCK

--- source/searchproducts.js
+++ source/searchproducts.js
@@ -23,4 +23,14 @@ document.addEventListener('DOMContentLoaded', function() {
   // Attach event listener to search input
   searchInput.addEventListener('input', handleSearch);
 
+  // Integrated navigation function
+  (function() {
+    var t, e;
+    t = document.querySelector('.nav-button');
+    e = document.querySelector('.navigation');
+    t.addEventListener('click', function() {
+      return e.classList.toggle('open');
+      t.classList.toggle('active');
+    }, false);
+  }).call(this);
 });
